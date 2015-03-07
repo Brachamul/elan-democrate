a916f053684d56339bcfb6c0a36f86f3fa712ce5
Brachamul <barnaby.brachamul@gmail.com>
2015-03-07 15:41:10 +0100

    changed location of auth template

--- membres/views.py
+++ membres/views.py
@@ -47,11 +47,11 @@ def enregistrement(request):
 
 	# If it's a HTTP POST, we're interested in processing form data.
 	if request.method == 'POST':
-		numero_adherent_ou_email = request.POST.get('numero_adherent_ou_email')
+		numero_ou_email = request.POST.get('numero_ou_email')
 
 		# regardons si c'est un numéro adhérent (donc isdigit) ou une adresse mail (avec un @)
-		if numero_adherent_ou_email.isdigit() :
-			num_adhérent = numero_adherent_ou_email
+		if numero_ou_email.isdigit() :
+			num_adhérent = numero_ou_email
 			# Cela ressemble à un numéro adhérent !
 			try : user = User.objects.get(username=num_adhérent)
 			except User.DoesNotExist :
@@ -69,14 +69,14 @@ def enregistrement(request):
 						messages.error(request, "Notre fichier adhérent ne contient pas d'adresse email valide pour ce numéro adhérent. Nous ne pouvons donc pas vous authentifier.")
 			else : messages.info(request, "Cet adhérent est déjà enregistré sur Élan Démocrate.")
 
-# !		elif "@" in numero_adherent_ou_email :
+# !		elif "@" in numero_ou_email :
 #			# Cela ressemble à une adresse mail
-#			email = numero_adherent_ou_email
+#			email = numero_ou_email
 #			try : user = User.objects.get(email=email)
 #			except User.DoesNotExist :
 #				messages.error(request, "Nous ne connaissons pas cette adresse email.")
 #			else : username = user.username
-#		elif numero_adherent_ou_email == "" : pass
+#		elif numero_ou_email == "" : pass
 #		# on ne met pas de message d'erreur si l'utilisateur a directement cliqué sur le bouton de connexion sans remplir le champs
 #		else : messages.error(request, "Vous semblez avoir entré quelque chose qui n'est ni un numéro adhérent, ni une adresse mail...")
 #			user.save()
@@ -120,17 +120,17 @@ def connexion(request):
 
 		username = None # default to no username
 		code_sent = False
-		numero_adherent_ou_email = request.POST.get('numero_adherent_ou_email')
+		numero_ou_email = request.POST.get('numero_ou_email')
 		# regardons si c'est un numéro adhérent (donc isdigit) ou une adresse mail (avec un @)
-		if numero_adherent_ou_email.isdigit() :
-			username = numero_adherent_ou_email
-		elif "@" in numero_adherent_ou_email :
-			email = numero_adherent_ou_email
+		if numero_ou_email.isdigit() :
+			username = numero_ou_email
+		elif "@" in numero_ou_email :
+			email = numero_ou_email
 			try : user = User.objects.get(email=email)
 			except User.DoesNotExist :
 				messages.error(request, "Nous ne connaissons pas cette adresse email.")
 			else : username = user.username
-		elif numero_adherent_ou_email == "" : pass
+		elif numero_ou_email == "" : pass
 		# on ne met pas de message d'erreur si l'utilisateur a directement cliqué sur le bouton de connexion sans remplir le champs
 		else : messages.error(request, "Vous semblez avoir entré quelque chose qui n'est ni un numéro adhérent, ni une adresse mail...")
 
