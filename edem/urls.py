d55c9bfb14f3caf5215cc379d3318dfa4bb213c4
Barnaby Brachamul <barnaby.brachamul@gmail.com>
2015-07-27 19:34:57 +0200

    removing accueil part 2

--- edem/urls.py
+++ edem/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
+from aggregateur.views import aggregateur
 
 admin.site.site_header = 'Élan Démocrate - Administration'
 
 urlpatterns = patterns('',
-	url(r'^$', RedirectView.as_view(permanent=False, url='/accueil/')),
-	url(r'^accueil/', include('accueil.urls')),
+	url(r'^$', aggregateur, name='accueil'),
 	url(r'^m/', include('membres.urls')),
 	url(r'^p/', include('aggregateur.urls')),
 	url(r'^n/', include('notifications.urls')),
@@ -15,7 +15,6 @@ urlpatterns = patterns('',
 	url(r'^fichiers-adherents/', include('fichiers_adherents.urls')),
 	url(r'^mandats/', include('mandats.urls')),
 	url(r'^admin/', include(admin.site.urls), name='admin'),
-
 )
 
 from django.conf import settings
