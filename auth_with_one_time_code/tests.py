13402a2295149d1baf18e571e7817f44c9612d6e
Barnaby Brachamul <barnaby.brachamul@gmail.com>
2015-12-15 02:16:18 +0100

    started building a test

---
+++ auth_with_one_time_code/tests.py
@@ -0,0 +1,38 @@
+import random
+
+from django.core import mail
+from django.test import TestCase, RequestFactory
+
+from autofixture import AutoFixture
+
+from django.contrib.auth.models import User
+from fichiers_adherents.models import Adherent
+
+from django.contrib.messages.storage.fallback import FallbackStorage
+
+#	from models import *
+from .views import enregistrement
+
+class RegistrationTestCases(TestCase):
+
+	def setUp(self):
+
+		self.factory = RequestFactory()
+		self.adherents = AutoFixture(Adherent).create(100) # On crée 100 faux adhérents
+
+	def test_registering_new_user_from_existing_adherent(self):
+		for adherent in self.adherents :
+			# On teste au pif le mail ou le numéro
+			if random.choice([True, False]) :
+				key = adherent.email
+			else :
+				key = adherent.num_adhérent
+
+			request = self.factory.post('/auth/enregistrement', data={'numero_ou_email': 'key'})
+			setattr(request, 'session', 'session')
+			messages = FallbackStorage(request)
+			setattr(request, '_messages', messages)
+
+			enregistrement(request)
+
+		print(mail.outbox)
